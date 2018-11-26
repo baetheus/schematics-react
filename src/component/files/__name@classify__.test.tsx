058508cb436811950727bfc8bafb2293d38f38a3
Brandon Blaylock <brandon@null.pub>
2018-11-26 08:27:41 -0800

    Fix for jest test templates to use typescript

--- src/component/files/__name@classify__.test.tsx
+++ src/component/files/__name@classify__.test.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
 
 import { <%= classify(name) %> } from './<%= classify(name) %>';
 
 test('<%= classify(name) %> changes the class when hovered', () => {
-  const component = renderer.create(
-    <<%= classify(name) %> />,
-  );
+  const component = renderer.create(<<%= classify(name) %> text="Hello World" />);
   let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
 
-  // manually trigger the callback
+  if (tree === null) {
+    throw new Error('component.toJSON returned null');
+  }
+
+  expect(tree).toMatchSnapshot();
   tree.props.onMouseEnter();
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  tree.props.onMouseLeave();
-  // re-rendering
-  tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
